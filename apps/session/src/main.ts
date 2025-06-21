a6734c2ec26e1e03318866335049af5149a67b14
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-06-22 00:06:15 +0300

    Refactor

--- apps/session/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { SessionModule } from './session.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(SessionModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
