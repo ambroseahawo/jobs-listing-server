cae2e50e491f4a5303fffa6072318b3e8aaf8fb2
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-06-23 21:31:57 +0300

    Implement app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,42 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
+import {
+  ThrottlerGuard,
+  ThrottlerModule,
+  ThrottlerModuleOptions,
+} from "@nestjs/throttler";
+
+import { validate } from "@config/env.validation";
+
+import { AuthModule } from "@modules/auth/auth.module";
+import { DatabaseModule } from "@modules/database/database.module";
+import { SessionModule } from "@modules/session/session.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate }),
+    ThrottlerModule.forRootAsync({
+      useFactory: (configService: ConfigService): ThrottlerModuleOptions => ({
+        throttlers: [
+          {
+            ttl: Number(configService.getOrThrow("UPLOAD_RATE_TTL")),
+            limit: Number(configService.getOrThrow("UPLOAD_RATE_LIMIT")),
+          },
+        ],
+      }),
+      inject: [ConfigService],
+    }),
+    DatabaseModule,
+    AuthModule,
+    SessionModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+require("module-alias/register");
+
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+
+import { AllExceptionsFilter } from "@common/global-filters/all-exceptions.filter";
+import { AppModule } from "@src/app.module";
+// import { runMigrations } from "@src/migrations/migration-runner";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix("/api/v1");
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>("APP_PORT");
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost, configService));
+
+  // await runMigrations();
+  await app.listen(port as string);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.log("Error during app bootstrap: ", err);
+  process.exit(1);
+});
